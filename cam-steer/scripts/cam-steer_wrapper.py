c29412aa4369fb9464c943db2259bd0324527b49
Eliacus <eliasneme2@gmail.com>
2019-11-27 10:16:35 +0100

    bugfixing

--- cam-steer/scripts/cam-steer_wrapper.py
+++ cam-steer/scripts/cam-steer_wrapper.py
@@ -1,9 +1,10 @@
  #!/usr/bin/env python
 
-from geometry.msgs.msg import Twist
+#from geometry.msgs.msg import Twist
 #from sensor_msgs.msg import CompressedImage
 
 import rospy
+from std_msgs.msg import String
 
 
 # CompressedImage message
@@ -24,15 +25,16 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "The cmd_vel is:  %s", data.data)
 
 def node():
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    pub = rospy.Publisher('cmd_vel', String, queue_size=1)
     rospy.init_node('cam_steer')
-    rospy.subscriber('cmd_vel', Twist, callback)
+    rospy.subscriber('cmd_vel', String, callback)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        twist = Twist()
-        twist.linear.x = 1
-        tist.angular.z = 1
-        pub.publish(twist)
+        hello_str = "hello world %s" % rospy.get_time()
+        #twist = Twist()
+        #twist.linear.x = 1
+        #tist.angular.z = 1
+        pub.publish(hello_str)
         rate.sleep()
 
 
