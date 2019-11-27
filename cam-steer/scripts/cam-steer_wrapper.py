34d19f1c1eabd21508d873e7b11d05e871d4b473
Elias <eliasneme2@gmail.com>
2019-11-27 09:38:22 +0000

    writing twist to cmd_topic

--- cam-steer/scripts/cam-steer_wrapper.py
+++ cam-steer/scripts/cam-steer_wrapper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-#from geometry.msgs.msg import Twist
-#from sensor_msgs.msg import CompressedImage
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import CompressedImage
 
 import rospy
 from std_msgs.msg import String
@@ -22,19 +22,19 @@ from std_msgs.msg import String
 # Create publisher
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "The cmd_vel is:  %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + "The cmd_vel is:  %s", data)
 
 def node():
-    pub = rospy.Publisher('cmd_vel', String, queue_size=1)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     rospy.init_node('cam_steer')
-    rospy.subscriber('cmd_vel', String, callback)
+    rospy.Subscriber('cmd_vel', Twist, callback)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        #twist = Twist()
-        #twist.linear.x = 1
-        #tist.angular.z = 1
-        pub.publish(hello_str)
+        #hello_str = "hello world %s" % rospy.get_time()
+        twist = Twist()
+        twist.linear.x = 1
+        twist.angular.z = 1
+        pub.publish(twist)
         rate.sleep()
 
 
