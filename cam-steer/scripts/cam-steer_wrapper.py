84d481e5ecee4d3c06a119cda9210ffacb93a8ca
Eliacus <eliasneme2@gmail.com>
2019-12-10 15:27:01 +0000

    local bugfixing

--- cam-steer/scripts/cam-steer_wrapper.py
+++ cam-steer/scripts/cam-steer_wrapper.py
@@ -1,44 +1,38 @@
 #!/usr/bin/env python
 
+# Message imports
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
 from std_msgs.msg import Float32
-from camera_algo import *
 
+# Import packages
+from cv_bridge import CvBridge
 import rospy
 import numpy as np
 import cv2 as cv
 import time
 
-# CompressedImage message
-#std_msgs/Header header
-#  uint32 seq
-#  time stamp
-#  string frame_id
-# string format
-# uint8[] data
-
-
-# Create subscriber
-
-
-# Create publisher
+from src.camera_algo import *
 
 def callback(data):
-    start_time = time.time()
 
-    # Convert image buffer
-    np_arr = np.fromstring(data.data, np.uint8)
-    im = cv.imdecode(np_arr, 0)
+    # Compressded Image
+    #np_arr = np.fromstring(data.data, np.uint8)
+    #im = cv.imdecode(np_arr, 0)
 
-    # Use im to calculate yaw
+    # Regular image
+    im = bridge.imgmsg_to_cv2(data, "mono8")
+
+    #rospy.loginfo(rospy.get_caller_id() + "The converted Image Data is:    %s", np.size(im,1))
+    #cv.imshow("image",im)
+    #cv.waitKey(3)
+
     tracker.run(im)
-    pub.publish(tracker.euler_angles[1])    # Change this to yaw variable
-
-    rospy.loginfo(rospy.get_caller_id() + "Yaw:    %s", tracker.euler_angles[1])
 def node():
     rospy.init_node('yaw_estimator')
-    rospy.Subscriber('raspicam_node/image/compressed', CompressedImage, callback)
+    rospy.Subscriber('raspicam_node/image', Image, callback)
+    #rospy.Subscriber('raspicam_node/image/compressed', CompressedImage, callback)
     rate = rospy.Rate(100) # 10hz
 
     while not rospy.is_shutdown():
@@ -48,13 +42,11 @@ def node():
 if __name__ == '__main__':
     try:
         pub = rospy.Publisher('cam_yaw', Float32, queue_size=1)
+        bridge = CvBridge()
         tracker = FeatureTracker()
         node()
     except rospy.ROSInterruptException:
         pass
-
-
-
 
 
 
