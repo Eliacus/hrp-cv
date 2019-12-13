c02409a044aceb964d80030e83098a24d25e6df1
Eliacus <eliasneme2@gmail.com>
2019-12-13 11:21:27 +0100

    Adding sensor fusion capabilities to controller

--- cam-steer/scripts/cam-controller_wrapper.py
+++ cam-steer/scripts/cam-controller_wrapper.py
@@ -5,40 +5,77 @@ from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import Float32
 from nav_msgs.msg import Odometry
 
-from camera_algo import *
 import numpy as np
 import rospy
+
+
+from camera_algo import *
 from cam_controller import Controller
+from sensor_fusion import SensorFusion
 
-
-def callback(data):
-    vel = controller.update(float(data.data))
+def cam_callback(data):
+    # Get
+    cam_yaw = controller.update(float(data.data))
     print("velocity command: ", vel)
     print(" Angle : ", np.rad2deg(float(data.data)))
+
+    fusion_filter.take_step(cam_yaw,last_odom)
+
     twist = Twist()
-    twist.angular.z = vel
+    twist.angular.z = fusion_filter.x
+
     twist.linear.x = 0.1
 
     pub.publish(twist)
 
-def node():
-    rospy.init_node('cam_ctrl')
-    rospy.Subscriber('cam_yaw', Float32, callback)
+def odom_callback(data):
 
+    last_odom = data.twist.twist.angular.z
+
+def init_node():
+        # Discrete time step
+        Ts = 0.1
+
+        # Initialize PID controller
+        P = 0.5
+        I = 0
+        D = 0
+        controller = Controller(P,I,D,Ts)
+
+        # Initialize sensor fusion algorithm
+        x_0 = 0
+        P_0 = 0
+        Q = np.ndarray([[1, 0],[0, 1]])
+        R_c = 1
+        R_0 = 10
+
+        fusion_filter = SensorFusion(x_0,P_0,Q,R_c,R_0,Ts)
+
+        # Initialize ros publisher
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+        # Initialize controller node
+        rospy.init_node('cam_ctrl')
+
+        # Save last odometer reading
+        last_odom = 0;
+
+
+def node():
+    rospy.Subscriber('cam_yaw', Float32, cam_callback)
+    rospy.Subscriber('odom',Odometry, odom_callback)
     rate = rospy.Rate(10) # 10hz
 
     while not rospy.is_shutdown():
         rate.sleep()
 
 
+
+
+
 if __name__ == '__main__':
     try:
-        P = 0.5
-        I = 0
-        D = 0
-        Ts = 0.1
-        controller = Controller(P,I,D,Ts)
-        pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
         node()
     except rospy.ROSInterruptException:
         pass
