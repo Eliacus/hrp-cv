fixing sign issue

cam-steer/scripts/cam-steer_wrapper.py
@@ -14,10 +14,10 @@ import cv2 as cv
 
 
 def callback(data):
-
     # Compressded Image
     np_arr = np.fromstring(data.data, np.uint8)
     im = cv.imdecode(np_arr, 0)
+
     # Regular image
     #im = bridge.imgmsg_to_cv2(data, "mono8")
 
@@ -25,8 +25,8 @@ def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + "The converted Image Data is:    %s", np.size(im,1))
     cv.imshow("image",im)
     cv.waitKey(3)
-    tracker.run(im)
 
+    tracker.run(im)
 def node():
     print("initating node")
     pub = rospy.Publisher('cam_yaw', Float32, queue_size=1)
@@ -35,10 +35,9 @@ def node():
     rospy.Subscriber('raspicam_node/image/compressed', CompressedImage, callback)
 
     rate = rospy.Rate(10) # 10hz
- 
     while not rospy.is_shutdown():
-	
-        pub.publish(tracker.euler_angles[1])    # Change this to yaw variable
+        pub.publish(-tracker.euler_angles[1])    # Change this to yaw variable
+
         rate.sleep()
 
 